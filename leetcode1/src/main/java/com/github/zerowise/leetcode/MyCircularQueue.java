aaf3141a02066a3a8ecdac5ef191c653e66af624
hanyuanliang <hanyuanliang@hoolai.com>
2018-10-08 12:58:00 +0800

    leetcode

---
+++ leetcode1/src/main/java/com/github/zerowise/leetcode/MyCircularQueue.java
@@ -0,0 +1,98 @@
+package com.github.zerowise.leetcode;
+
+/**
+ ** @createtime : 2018/10/8下午12:14
+ **/
+public class MyCircularQueue {
+
+    private int head = -1;// 头部指针
+    private int tail = -1;//尾部指针
+    private int[] nodeArray;//数组
+    private int size;
+    private int capacity;
+
+    /** Initialize your data structure here. Set the size of the queue to be k. */
+    public MyCircularQueue(int k) {
+        capacity = k;
+        nodeArray = new int[capacity];
+    }
+
+    /** Insert an element into the circular queue. Return true if the operation is successful. */
+    public boolean enQueue(int value) {
+        if (isFull()) {
+            return false;
+        }
+
+        if (head == -1) {
+            head = 0;
+        }
+        tail = (tail + 1) % capacity;
+        nodeArray[tail] = value;
+        size++;
+        return true;
+    }
+
+    /** Delete an element from the circular queue. Return true if the operation is successful. */
+    public boolean deQueue() {
+        if (isEmpty()) {
+            return false;
+        }
+
+        nodeArray[head] = 0;
+        size--;
+        if (head == tail) {
+            head = tail = -1;
+            return true;
+        }
+        head = (head + 1) % capacity;
+        return true;
+    }
+
+    /** Get the front item from the queue. */
+    public int Front() {
+        if (isEmpty())
+            return -1;
+        return nodeArray[head];
+    }
+
+    /** Get the last item from the queue. */
+    public int Rear() {
+        if (isEmpty())
+            return -1;
+        return nodeArray[tail];
+    }
+
+    /** Checks whether the circular queue is empty or not. */
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    /** Checks whether the circular queue is full or not. */
+    public boolean isFull() {
+//        return (tail + 1) % nodeArray.length == head;
+        return size == capacity;
+    }
+
+
+    public static void main(String[] args) {
+        MyCircularQueue circularQueue = new MyCircularQueue(3); // 设置长度为3
+
+        circularQueue.enQueue(1);  // 返回true
+
+        circularQueue.enQueue(2);  // 返回true
+
+        circularQueue.enQueue(3);  // 返回true
+
+        circularQueue.enQueue(4);  // 返回false,队列已满
+
+        circularQueue.Rear();  // 返回3
+
+        circularQueue.isFull();  // 返回true
+
+        circularQueue.deQueue();  // 返回true
+
+        circularQueue.enQueue(4);  // 返回true
+
+        circularQueue.Rear();  // 返回4
+    }
+}
--- leetcode1/src/main/java/com/github/zerowise/leetcode/Solution.java
+++ leetcode1/src/main/java/com/github/zerowise/leetcode/Solution.java
@@ -2,19 +2,20 @@ package com.github.zerowise.leetcode;
 
 import java.util.Arrays;
 
-/**
- * 爱丽丝有一手（hand）由整数数组给定的牌。
- *
- * 现在她想把牌重新排列成组，使得每个组的大小都是 W，且由 W 张连续的牌组成。
- *
- * 如果她可以完成分组就返回 true，否则返回 false。
- */
+
 public class Solution {
+
+    /**
+     * 爱丽丝有一手（hand）由整数数组给定的牌。
+     *
+     * 现在她想把牌重新排列成组，使得每个组的大小都是 W，且由 W 张连续的牌组成。
+     *
+     * 如果她可以完成分组就返回 true，否则返回 false。
+     */
     public static boolean isNStraightHand(int[] hand, int w) {
         if (w == 1) {
             return hand.length >= w;
         }
-        Arrays.sort(hand);
         int firstEleIndex = 0;
         int num = 0;
         while (firstEleIndex + w <= hand.length) {
@@ -27,7 +28,7 @@ public class Solution {
                     tmpArray[tempInTempIndex + 1] = hand[tempInHandsIndex];
                     tempInTempIndex++;
                     if (tempInTempIndex == w - 1) {
-                        num += 2;
+                        num++;
                         if (num >= w) {
                             return true;
                         }
@@ -43,6 +44,26 @@ public class Solution {
 
 
     public static void main(String[] args) {
-        System.out.println(isNStraightHand(new int[]{2,1}, 2));
+        System.out.println(isNStraightHand(new int[]{2, 1}, 2));
+    }
+
+
+    /**
+     * 给定一个整数数组和一个目标值，找出数组中和为目标值的两个数。
+     *
+     * 你可以假设每个输入只对应一种答案，且同样的元素不能被重复利用。
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return new int[0];
     }
 }
