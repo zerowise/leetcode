e37f158e1d591485ab60e9ac106f69e62aaa105f
hanyuanliang <hanyuanliang@hoolai.com>
2018-10-08 18:55:41 +0800

    leetcode

--- leetcode1/src/main/java/com/github/zerowise/leetcode/Solution.java
+++ leetcode1/src/main/java/com/github/zerowise/leetcode/Solution.java
@@ -233,4 +233,64 @@ public class Solution {
 //
 //        }
 //    }
+
+
+    /**
+     * 给定两个大小为 m 和 n 的有序数组 nums1 和 nums2 。
+     *
+     * 请找出这两个有序数组的中位数。要求算法的时间复杂度为 O(log (m+n)) 。
+     *
+     * 你可以假设 nums1 和 nums2 不同时为空。
+     *
+     * 示例 1:
+     *
+     * nums1 = [1, 3]
+     * nums2 = [2]
+     *
+     * 中位数是 2.0
+     * 示例 2:
+     *
+     * nums1 = [1, 2]
+     * nums2 = [3, 4]
+     *
+     * 中位数是 (2 + 3)/2 = 2.5
+     * @param nums1
+     * @param nums2
+     * @return
+     *
+     *
+     *  see https://blog.csdn.net/hk2291976/article/details/51107778
+     */
+
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int len1 = nums1.length;
+        int len2 = nums2.length;
+        if (len1 > len2) {
+            return findMedianSortedArrays(nums2, nums1);
+        }
+
+        int lo = 0, hi = 2 * len1;//加入了类似manacher算法的虚拟元素，目的是保持一般性
+        int l1 = 0, r1 = 0, l2 = 0, r2 = 0;
+        while (lo <= hi) {
+            int c1 = (lo + hi) / 2;
+
+            // 按前k个数字给的割点，那么对于中位数k = m + n
+            int c2 = len2 + len1 - c1;
+
+            l1 = c1 == 0 ? Integer.MIN_VALUE : nums1[(c1 - 1) / 2];
+            r1 = c1 == 2 * len1 ? Integer.MAX_VALUE : nums1[c1 / 2];
+
+            l2 = c2 == 0 ? Integer.MIN_VALUE : nums2[(c2 - 1) / 2];
+            r2 = c2 == 2 * len2 ? Integer.MAX_VALUE : nums2[c2 / 2];
+
+            if (l1 > r2) {
+                hi = c1 - 1;
+            } else if (l2 > r1) {
+                lo = c1 + 1;
+            } else {
+                break;
+            }
+        }
+        return (Math.max(l1, l2) + Math.min(r1, r2)) / 2.0;
+    }
 }
